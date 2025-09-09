74c442c9c1f62059c8e1756a177eb03aa18481a9
KJRAVAL32 <kuldeepr.reliution@gmail.com>
2025-09-09 15:40:10 +0530

    Update programs3.py

--- programs3.py
+++ programs3.py
@@ -28,8 +28,8 @@ class movement(products):
         else:
             product.stock_at_locations[move.from_location]-=move.quantity
             product.stock_at_locations[move.to_location]+=move.quantity
-            print("Stock at locations:")
-        print(f"{product.name}:{product.stock_at_locations}")
+        print("Stock at locations:")
+        print(f"{product.name}-{product.code}:{product.stock_at_locations}")
         print("---------------------------------------------------------------------")
 loct1=location("warehouse","101")
 loct2=location("store1","102")
@@ -39,16 +39,19 @@ prod1=products("laptop","001","electric things","60000",{'warehouse':500,'store1
 prod2=products("Monitor","002","electric things","100000",{'store1':200,'store3':10})
 prod3=products("cpu","003","electric things","400000",{'store2':20,'store3':100})
 prod4=products("graphic card","004","electric things","200000",{'warehouse':900,'store1':100})
-prod5=products("Hard disk","005","electric things","10000",{'warehouse':200,'store2':10})
+prod5=products("Hard disk","005","electric things","10000",{'warehouse':200,'store2':100,'store3':150,'store1':10})
 # movements=[
 move1=movement("warehouse","store1","laptop",10)
 move6=movement("warehouse","store1","laptop",5)
 move2=movement("store1","store3","monitor",20)
 move3=movement("store2","store3","cpu",20)
 move4=movement("warehouse","store1","graphic card",10)
-move5=movement("warehouse","store2","hard disk",200)
+move5=movement("store1","warehouse","hard disk",200)
 # ]
 print("Product Movements:")
 movement.movement_by_product(prod1,move1)
 movement.movement_by_product(prod2,move2)
-movement.movement_by_product(prod1,move6)
+movement.movement_by_product(prod3,move3)
+movement.movement_by_product(prod4,move4)
+movement.movement_by_product(prod5,move5)
+movement.movement_by_product(prod5,move6)
