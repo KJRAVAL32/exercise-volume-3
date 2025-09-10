02b1e08f3b6e15d7f198411e7438c28439c33845
KJRAVAL32 <kuldeepr.reliution@gmail.com>
2025-09-10 09:51:50 +0530

    Update programs3.py

--- programs3.py
+++ programs3.py
@@ -34,13 +34,12 @@ loc2 = location("store1", "102")
 loc3 = location("store2", "103")
 loc4 = location("store3", "104")
 locations = [loc1, loc2, loc3, loc4]
-prod1 = products("laptop", "001", "electric things", "60000", {'warehouse': 500, 'store1': 0})
-prod2 = products("Monitor", "002", "electric things", "100000", {'store1': 200, 'store3': 10})
-prod3 = products("cpu", "003", "electric things", "400000", {'store2': 20, 'store3': 100})
-prod4 = products("graphic card", "004", "electric things", "200000", {'warehouse': 900, 'store1': 100})
-prod5 = products("Hard disk", "005", "electric things", "10000", {'warehouse': 200, 'store2': 100, 'store3': 150, 'store1': 10})
+prod1 = products("laptop", "001", "electric things", "60000", {loc1: 500, loc2: 0})
+prod2 = products("Monitor", "002", "electric things", "100000", {loc2: 200, loc4: 10})
+prod3 = products("cpu", "003", "electric things", "400000", {loc3: 20, loc4: 100})
+prod4 = products("graphic card", "004", "electric things", "200000", {loc1: 900, loc2: 100})
+prod5 = products("Hard disk", "005", "electric things", "10000", {loc1: 200, loc3: 100, loc4: 150, loc2: 10})
 po = [prod5, prod4, prod3, prod2, prod1]
-# movements=[
 move1 = movement(loc1, loc2, prod1, 10)
 move6 = movement(loc1, loc2,  prod1, 5)
 move2 = movement(loc2, loc4, prod2, 20)
@@ -48,7 +47,6 @@ move3 = movement(loc3, loc4, prod3, 20)
 move4 = movement(loc1, loc2, prod4, 10)
 move5 = movement(loc2, loc1, prod5, 200)
 movements = [move5, move4, move3, move2, move1]
-# ]
 print("Product Movements:")
 movement.movement_by_product(prod1, move1)
 movement.movement_by_product(prod2, move2)
@@ -60,5 +58,5 @@ for i in locations:
     print("--------------------------------------")
     print(i.name, i.code, ":")
     for j in po:
-        if i.name in j.stock_at_locations:
-            print(j.name,j.stock_at_locations[i.name])
+        if i in j.stock_at_locations.keys():
+            print(j.name,j.stock_at_locations[i])
